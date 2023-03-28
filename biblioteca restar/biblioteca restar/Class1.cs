03683d309deac68bacff45e40055b75358de2dc2
JuanDavid05Sa <110199712+JuanDavid05Sa@users.noreply.github.com>
2023-03-28 11:16:27 -0500

    Add files via upload

---
+++ biblioteca restar/biblioteca restar/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace biblioteca_restar
+{
+    /// <summary>
+    /// Metodo para Restar
+    /// </summary>
+    /// <param name="i">Operacion 1</param>
+    /// <param name="j">Operacion 2</param>
+    /// <returns></returns>
+    public class Class1
+    {
+        public static long Restar(long i, long j)
+        {
+            return (i-j);
+        }
+    }
+}
