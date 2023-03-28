6c1dbd6977d1cc27a5468f6d22bb5f5527651822
Juan-Fer-Vergara <112653261+Juan-Fer-Vergara@users.noreply.github.com>
2023-03-28 18:17:34 +0200

    Add files via upload

---
+++ nacho division/nacho division/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nacho_division
+{
+    public class Class1
+    {
+        /// <summary>
+        /// metodo multiplicar
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static long division (long a, long b)
+        {
+            return (a / b);
+        }
+
+    }
+}
