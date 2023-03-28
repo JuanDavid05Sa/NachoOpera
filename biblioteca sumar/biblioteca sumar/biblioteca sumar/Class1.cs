65e0d2cc6a74fac7c9118e8a004a28f5cfad1e84
Estefania1907 <106752800+Estefania1907@users.noreply.github.com>
2023-03-28 11:17:43 -0500

    Add files via upload

---
+++ biblioteca sumar/biblioteca sumar/biblioteca sumar/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace biblioteca_sumar
+{
+    ///<sumary>
+    ///Metodo para sumar
+    ///</sumary>
+    ///<param name="x">Operando 1</param>
+    ///<param name="z">Operando 2</param>
+    ///<Returns></Returns>
+    public class Class1
+    {
+        public static long Suma(int x, int z)
+        {
+            return(x+z);
+        }
+    }
+}
