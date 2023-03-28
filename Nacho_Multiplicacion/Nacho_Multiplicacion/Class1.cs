f1f7e289c865c7898220b68260167625ffae29a1
turizoj <110200075+turizoj@users.noreply.github.com>
2023-03-28 11:21:44 -0500

    Add files via upload

---
+++ Nacho_Multiplicacion/Nacho_Multiplicacion/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nacho_Multiplicacion
+{
+    public class Class1
+    {
+        ///<summary>
+        /// Metodo Multiplicacion
+        /// </summary>
+        /// <param name="a"> operando 1 </param>
+        /// <param name="b"> operando 2 </param>
+        /// <returns></returns>
+        public static long Multiplicacion (long a, long b)
+        {
+            return (a * b);
+        }
+    }
+}
